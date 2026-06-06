00247179a36fbe8463369779a6f3caa809e4a3db
foongwei2267-netizen <foongwei2267@gmail.com>
2026-06-06 21:42:44 +0800

    Delete forscend directory

--- forscend/js/scriptTest.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-
-const Menu = document.querySelector('.menu');
-
-hamMenu.addEventListener('click', () => {
-  hamMenu.classList.toggle('active');
-  Menu.classList.toggle('active');
-})
